6d98f82b27dc88b5a121c43da2d12189609bb437
Anuj Panchal <anujpan1@gmail.com>
2018-09-20 20:27:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,8 @@ def prime?(num)
   if num <= 1
     false
   else
-    (2..num/2).none?
+    (2..num/2).none? do
+      
   end
   
   # from 2..num remove every X value and check if num is in the list
